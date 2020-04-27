e2bf769b0dfff32256b5e747de0b6a8e103cd458
Your NameMChau105 <you@example.combdmchau10005@gmail.com>
2020-04-27 16:41:33 +0700

    first upload

--- dateFormat/date.js
+++
@@ -1,8 +0,0 @@
-export const dateFormat = {
-    "months": [
-        "January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", " November", "December"
-    ],
-    "days": [
-        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-    ]
-}
